b216d44d40f172324a242725d5547f9d6b15f8d5
cclivick <46388110+cclivick@users.noreply.github.com>
2019-04-27 17:05:42 -0500

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,10 @@
 var characterWasChosen;
 var chosenCharHealth;
 var opponentHealth;
+var attackMultiplier = 0;
+var currentPower;
+var opponentAttack;
+
 //Add click event to #character elements that moves clicked character to .yourCharacter div//
 $( document ).ready(function() {
 
@@ -10,7 +14,6 @@ $( document ).ready(function() {
         $(this).css({"background-color" : "rgb(1, 80, 1)", "border" : "3px solid white"});
         $(this).appendTo($(".yourCharacter"));
         chosenCharHealth = +$('.yourCharacter .charPoints').text();
-        console.log(chosenCharHealth);
         // adding plus sign (+) before "str" will presumably convert to number
         characterWasChosen = 1;
 //Once #character element clicked, move other #character elements to .yourEnemies div//    
@@ -28,28 +31,75 @@ $( document ).ready(function() {
         $(this).appendTo($(".yourOpponent"));
         $(this).css({"background-color" : "black", "border" : "3px solid red"});
         opponentHealth = +$('.yourOpponent .charPoints').text();
+        opponentChosen = 1;
         console.log(opponentHealth)
 //Add click event to attackButton that decreases opponentHealth and chosenCharHealth//
 //WHY IS THIS CLICK FUNCTION ADDING +$('.yourOpponent .charPoints').text() to var chosenCharHealth?
+        // ANSWERED: Click functions sometimes need to be removed from divs as well. In this case, the classes were removed, but the click function from .availChar was "overriding" it. Once the .off method was added on line 24, it fixed the issue.  
     
-    $( ".attackButton" ).click(function() {
-        console.log($( ".opponent" ).attr("id"))
+    $( ".attackButton" ).on("click", function() {
+        attackMultiplier++;
+        opponentHealth -= 11 * attackMultiplier;
+        currentPower = 11 * attackMultiplier;
+        console.log(opponentHealth);
+        if(opponentHealth <= 0) {
+            $(".opponent").remove();
+            $( ".fightStats .oppoCounter").text("Your opponent was defeated!");
+            $( ".fightStats .yourAttack").text("You attacked for " + currentPower + " damage!") 
+        }
+
+        $( ".fightStats .yourAttack").text("You attacked for " + currentPower + " damage!")
+        $( ".yourOpponent .charPoints" ).text(opponentHealth);
+
         if($( ".opponent" ).attr("id") === "scorpion") {
             chosenCharHealth -= 25;
+            if(chosenCharHealth <= 0) {
+                $(".fightStats .yourAttack").text("You Lose! Fatality!");
+                $(".fightStats .oppoCounter" ).text("FATALITY!");
+            } else {
             $( ".yourCharacter .charPoints").text(chosenCharHealth);
+            $( ".fightStats .oppoCounter").text("Your opponent countered for 25 damage!")
+            }
+
         } else if ($( ".opponent" ).attr("id") === "subZero") {
-            chosenCharHealth -= 30;
-            $( ".yourCharacter .charPoints").text(chosenCharHealth);
-        } else if ($( ".opponent" ).attr("id") === "raiden") {
             chosenCharHealth -= 35;
+            if(chosenCharHealth <= 0) {
+                $(".fightStats .yourAttack").text("You Lose! Fatality!");
+                $(".fightStats .oppoCounter" ).text("FATALITY!");
+            } else {
             $( ".yourCharacter .charPoints").text(chosenCharHealth);
+            $( ".fightStats .oppoCounter").text("Your opponent countered for 35 damage!")
+            }
+
+        } else if ($( ".opponent" ).attr("id") === "raiden") {
+            chosenCharHealth -= 50;
+            if(chosenCharHealth <= 0) {
+                $(".fightStats .yourAttack").text("You Lose! Fatality!");
+                $(".fightStats .oppoCounter" ).text("FATALITY!");
+            } else {
+            $( ".yourCharacter .charPoints").text(chosenCharHealth);
+            $( ".fightStats .oppoCounter").text("Your opponent countered for 50 damage!")
+            }
 
         } else if ($( ".opponent" ).attr("id") === "liuKang") {
-            chosenCharHealth -= 25;
+            chosenCharHealth -= 35;
+            if(chosenCharHealth <= 0) {
+                $(".fightStats .yourAttack").text("You Lose!");
+                $(".fightStats .oppoCounter" ).text("FATALITY!");
+            } else {
             $( ".yourCharacter .charPoints").text(chosenCharHealth);
+            $( ".fightStats .oppoCounter").text("Your opponent countered for 35 damage!")
+            }
+            
         } else if ($( ".opponent" ).attr("id") === "shaoKhan") {
-            chosenCharHealth -= 50;
+            chosenCharHealth -= 65;
+            if(chosenCharHealth <= 0) {
+                $(".fightStats .yourAttack").text("You Lose!");
+                $(".fightStats .oppoCounter" ).text("FATALITY!");
+            } else {
             $( ".yourCharacter .charPoints").text(chosenCharHealth);
+            $( ".fightStats .oppoCounter").text("Your opponent countered for 65 damage!")
+            }
         }
         });
     });    
